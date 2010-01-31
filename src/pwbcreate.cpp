92a61949cfc04588c6220f39c7b2a22c33ccc3dd
Liu Liu <liuliu.1987@gmail.com>
2010-01-31 13:36:51 -0500

    modified:   src/pwbcreate.cpp

--- src/pwbcreate.cpp
+++ src/pwbcreate.cpp
@@ -4,9 +4,10 @@
 
 int main(int argc, char** argv)
 {
-	gsl_qrng* q = gsl_qrng_alloc(gsl_qrng_halton, 2);
 	IplImage* image = cvCreateImage(cvSize(320, 240), IPL_DEPTH_8U, 3);
 	cvNamedWindow("result", CV_WINDOW_AUTOSIZE);
+/*
+	gsl_qrng* q = gsl_qrng_alloc(gsl_qrng_halton, 2);
 	for (int i = 0; i < 64; i++)
 	{
 		double v[2];
@@ -14,9 +15,10 @@ int main(int argc, char** argv)
 		int x = (int)(v[0] * 319), y = (int)(v[1] * 239);
 		image->imageData[x * 3 + y * image->widthStep] = image->imageData[x * 3 + 1 + y * image->widthStep] = image->imageData[x * 3 + 2 + y * image->widthStep] = 255;
 	}
+	gsl_qrng_free(q);
+*/
 	cvShowImage("result", image);
 	cvWaitKey(0);
-	gsl_qrng_free(q);
 	cvDestroyWindow("result");
 	return 0;
 }
